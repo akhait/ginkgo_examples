f0ebf8f6a4e5b9cd8aecbcdbd18263daa294213a
Anna Khaitovich <akhaitovich@gmail.com>
2016-08-04 12:24:47 +0200

    Beautify main

--- main.go
+++ main.go
@@ -2,16 +2,24 @@ package main
 
 import (
 	"fmt"
-	"log"
-	//"ginkgo_examples/validation_testing"
-	"ginkgo_examples/validation_ginkgo"
+	//"log"
+	"ginkgo_examples/validation_testing"
+	//"ginkgo_examples/validation_ginkgo"
+	"ginkgo_examples/benchmarking"
 )
 
 func main() {
+	
+	greeting := validation_testing.RegisterNewUsername("Example_name")
+	fmt.Println(greeting)
+	/*
 	greeting, err := validation_ginkgo.RegisterNewUsername("Example_name")
 	if err != nil {
 		log.Fatal(err)
 	} else {
 		fmt.Println(greeting)
 	}
+	
+	fmt.Println(benchmarking.ReverseOrder("test"))
+	*/
 }
