99983499a023150dc678c04102e57e2e7f80fb60
Anna Khaitovich <akhaitovich@gmail.com>
2016-08-04 13:00:24 +0200

    Final changes

--- main.go
+++ main.go
@@ -5,15 +5,15 @@ import (
 	//"log"
 	"ginkgo_examples/validation_testing"
 	//"ginkgo_examples/validation_ginkgo"
-	"ginkgo_examples/benchmarking"
+	//"ginkgo_examples/benchmarking"
 )
 
 func main() {
-	
-	greeting := validation_testing.RegisterNewUsername("Example_name")
+	greeting := validation_testing.RegisterNewUsername("QA Community")
 	fmt.Println(greeting)
+	
 	/*
-	greeting, err := validation_ginkgo.RegisterNewUsername("Example_name")
+	greeting, err := validation_ginkgo.RegisterNewUsername("QA Community")
 	if err != nil {
 		log.Fatal(err)
 	} else {
